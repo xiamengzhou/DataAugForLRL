75dcb1e188b22e1fc65df5b71a5efa9be4e4e648
xiamengzhou <296337231@qq.com>
2018-11-19 00:27:45 -0500

    Fix bugs.

--- onmt/modules/__init__.py
+++ onmt/modules/__init__.py
@@ -2,6 +2,7 @@ from onmt.modules.Transformer import \
    TransformerEncoder, TransformerDecoder
 from onmt.modules.Embeddings import Embeddings, PositionalEncoding
 from onmt.modules.Generator import Generator
+from onmt.Models import NMTModel
 
 # For flake8 compatibility.
 __all__ = [NMTModel, PositionalEncoding, TransformerEncoder, TransformerDecoder, Embeddings, Generator]
