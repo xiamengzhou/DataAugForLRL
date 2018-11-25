5823949e347c3b46aa375ba71c108cbd881d0412
xiamengzhou <296337231@qq.com>
2018-11-24 23:29:10 -0500

    Added utils and extract embeddings.

--- py_scripts/extract_embeddings.py
+++ py_scripts/extract_embeddings.py
@@ -25,16 +25,16 @@ if __name__ == '__main__':
     for i, emb in enumerate(src_embedding):
         emb_str = [str(e) for e in emb]
         assert len(emb_str) == 512
-        embedding_output.write(" ".join(emb_str))
+        embedding_output.write("\t".join(emb_str))
         embedding_output.write("\n")
 
         w = src_vocab.itos[i]
         if w in lrl_vocab and w in hrl_vocab:
-            vocab_output.write(w + " " + "lrl+hrl" + "\n")
+            vocab_output.write(w + "\t" + "lrl+hrl" + "\n")
         elif w in lrl_vocab:
-            vocab_output.write(w + " " + "lrl" + "\n")
+            vocab_output.write(w + "\t" + "lrl" + "\n")
         else:
-            vocab_output.write(w + " " + "hrl" + "\n")
+            vocab_output.write(w + "\t" + "hrl" + "\n")
 
 
 
