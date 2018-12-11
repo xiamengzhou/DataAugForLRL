77998c9f2c9f30450ab0230f3a6d52462824107d
shawnkx <xiangk@andrew.cmu.edu>
2018-12-11 16:01:57 -0500

    update

---
+++ py_scripts/language_model/utils.py
@@ -0,0 +1,13 @@
+import os, shutil
+import torch
+
+def create_exp_dir(path, scripts_to_save=None):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    print('Experiment dir : {}'.format(path))
+    if scripts_to_save is not None:
+        os.mkdir(os.path.join(path, 'scripts'))
+        for script in scripts_to_save:
+            dst_file = os.path.join(path, 'scripts', os.path.basename(script))
+            shutil.copyfile(script, dst_file)
