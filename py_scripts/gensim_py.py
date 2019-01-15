f788b67f8f7917e7311bf707d262b30bbf7386b6
xiamengzhou <296337231@qq.com>
2019-01-14 22:38:48 -0500

    Added sentence piece.

--- py_scripts/gensim_py.py
+++ py_scripts/gensim_py.py
@@ -1,10 +1,13 @@
 """
-    python3 gensim.py
+    python3 ~/11731_final/py_scripts/gensim.py train /projects/tir3/users/mengzhox/data/unsup/mono/aztr/all.az-tr.spm8k \
+                      /projects/tir3/users/mengzhox/data/unsup/mono/aztr/emb/emb.spm8k \
+                      256 \
+                      /projects/tir3/users/mengzhox/data/unsup/mono/aztr/emb/emb.spm8k.vec
 """
 
 # Gensim
 def train_word2vec_while_fixing(train_file, pretrained="", size=256, export=""):
-    from gensim.models import Word2Vec
+    from gensim_py.models import Word2Vec
     sentences = open(train_file, "r").readlines()
     sentences = [s.split() for s in sentences]
     model = Word2Vec(size=size, min_count=0, negative=10, iter=10)
