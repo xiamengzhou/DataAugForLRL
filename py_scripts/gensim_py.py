c7ecbb944de0386300a1fdf897149b19b2bcbdca
xiamengzhou <296337231@qq.com>
2019-01-14 22:39:40 -0500

    Added sentence piece.

--- py_scripts/gensim_py.py
+++ py_scripts/gensim_py.py
@@ -7,7 +7,7 @@
 
 # Gensim
 def train_word2vec_while_fixing(train_file, pretrained="", size=256, export=""):
-    from gensim_py.models import Word2Vec
+    from gensim.models import Word2Vec
     sentences = open(train_file, "r").readlines()
     sentences = [s.split() for s in sentences]
     model = Word2Vec(size=size, min_count=0, negative=10, iter=10)
